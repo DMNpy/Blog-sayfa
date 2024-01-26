6fd92396f9a66921a50051e56bdc8655431045d8
DMN <102384385+DMNpy@users.noreply.github.com>
2024-01-27 02:19:04 +0300

    Update hakkimda.jsx

--- src/components/hakkimda.jsx
+++ src/components/hakkimda.jsx
@@ -1,38 +1,8 @@
 function Hakkimda() {
     return (
         <p className="hakkimda">
-            Merhaba, Ben Özcan Duman!
-
-        Biraz önce tanıştık, ancak şimdi biraz daha derine inelim. Adım Özcan, soyadım Duman. 28 yaşındayım ve Anadolu Üniversitesi'nde Yönetim Bilişim Sistemleri okuyorum. Bu süre zarfında, bilgisayar dünyasının gizemli koridorlarında dolaşarak ve teknolojinin nabzını tutarak kendimi geliştirmeye çalışıyorum.
-        
-        Eğitim Yolculuğum: Anadolu Üniversitesi
-        
-        Anadolu Üniversitesi, Yönetim Bilişim Sistemleri bölümünde geçirdiğim bu yıllar, hem teorik bilgilerle donanmamı sağladı hem de pratik becerilerimi geliştirmeme yardımcı oldu. Bilgisayar dünyasındaki hızlı değişimlere ayak uydurabilmek adına çeşitli projelerde yer aldım ve bu süreçte farklı teknoloji alanlarına olan ilgim her geçen gün arttı.
-        
-        Hobilerim ve Boş Zaman Aktivitelerim
-        
-        Öğrencilik hayatının yanı sıra, boş zamanlarımda keyifli aktivitelere yöneliyorum. Kitap okuma tutkum, farklı dünyalara açılan kapılar aralamama vesile oluyor. Fantezi romanlardan tarihi kitaplara kadar geniş bir yelpazede okuma alışkanlığı edindim.
-        
-        Bir diğer hobim ise bilgisayar oyunları oynamak. Sanal dünyalarda macera dolu yolculuklara çıkmak, strateji geliştirmek ve diğer oyuncularla etkileşimde bulunmak, benim için hem eğlenceli bir aktivite hem de sosyal bir deneyim.
-        
-        Spor ve Sağlıklı Yaşam Tutkum
-        
-        Yaklaşık 7 aydır düzenli spor yapmaya başladım. Fiziksel aktivitenin, zihinsel sağlığıma olan olumlu etkilerini keşfetmek benim için büyük bir motivasyon kaynağı oldu. Spor yaparken kendimi daha enerjik ve motive hissediyorum.
-        
-        Kendimi Geliştirme ve Sürekli Öğrenme İsteğim
-        
-        Hayatımın her alanında kendimi geliştirmeye açık bir şekilde ilerliyorum. Teknoloji alanındaki yenilikleri takip etmek, online kurslar ve kitaplar aracılığıyla öğrenme sürecimi sürdürmek, beni hem mesleki açıdan hem de kişisel olarak zenginleştiriyor.
-        
-        Geleceğe Yönelik Hedeflerim ve Blog Yolculuğum
-        
-        Gelecekte, aldığım eğitim ve birikimlerim doğrultusunda bilişim dünyasında etkili bir rol oynamayı hedefliyorum. Ayrıca, deneyimlerimi ve öğrendiklerimi paylaşarak, benzer ilgi alanlarına sahip insanlarla bağlantı kurmayı amaçlıyorum.
-        
-        Bu blog, sizlerle tecrübelerimi, öğrenme süreçlerimi ve ilgi alanlarımdaki gelişmeleri paylaşmak adına oluşturuldu. Burada, teknoloji, kitaplar, oyunlar, spor ve kişisel gelişim konularında yazılar bulabilirsiniz.
-        
-        Umarım blog yazılarımdan keyif alır, birlikte öğrenir ve paylaşırız. Yorumlarınızı bekliyorum!
-        
-        Sağlıcakla kalın,
-        Özcan Duman</p>
+            DMN
+        </p>
     );
 }
 
